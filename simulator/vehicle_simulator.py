8e2c95e23c7c0e29893fecc2eb176f4fb952ae8b
tkgoemae <t.goemaere@hotmail.com>
2016-03-20 21:23:32 +0100

    VehicleSimulator: stream_simulator commented out

--- simulator/vehicle_simulator.py
+++ simulator/vehicle_simulator.py
@@ -2,13 +2,12 @@ from dronekit import connect, VehicleMode
 import time
 from dronekit_sitl import SITL
 from subprocess import Popen
-from stream_simulator import StreamSimulator
+#from stream_simulator import StreamSimulator
 
 class VehicleSimulator():
     def __init__(self):
         self.vehicle = None
         self.sitl = None
-        self.video_stream = None
 
     def get_vehicle(self):
         if self.vehicle is None:
@@ -41,7 +40,3 @@ class VehicleSimulator():
         # Shut down simulator if it was started.
         if self.sitl is not None:
             self.sitl.stop()
-
-	if self.video_stream is not None:
-	    self.video_stream.kill()
-
