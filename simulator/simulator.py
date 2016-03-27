2db3f7714b36b43cc53bdcaffb95adff8e298f4d
Jonas <jonasleo.spanoghe@ugent.be>
2016-03-27 22:55:01 +0200

    convert stream simulator to vlc

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -2,10 +2,23 @@ import sys
 import time
 import signal
 from subprocess import Popen, PIPE
+from dronekit_sitl import SITL
+from stream_simulator import StreamSimulator
 
 
 class Simulator:
     def __init__(self):
+        # SITL is the system in the loop simulator from dronekit
+        self.sitl = SITL()
+        self.sitl.download('solo', '1.2.0', verbose=True)
+        sitl_args = ['solo', '--home=51.022627,3.709807,5,0']
+        self.sitl.launch(sitl_args, await_ready=True, restart=True)
+
+        # TODO: test if waiting for sitl here is necessary
+        time.sleep(2)
+
+        self.stream_simulator = StreamSimulator()
+
         self.server_process = Popen(['python2', '../onboard/server.py', '--level', 'debug', '--simulate'])
         self.control_process = Popen(['python2', '../onboard/control_module.py', '-level', 'debug', '-simulate'])
 
@@ -17,6 +30,7 @@ class Simulator:
         print "Exiting simulator"
         self.server_process.send_signal(sig=signal)
         self.control_process.send_signal(sig=signal)
+        self.sitl.stop()
         sys.exit(0)
 
 
--- simulator/stream_simulator.py
+++ simulator/stream_simulator.py
@@ -1,61 +1,27 @@
-import gi
-gi.require_version('Gst', '1.0')
-from gi.repository import GObject, Gtk
-from gi.repository import Gst as gst
+"""
+To be able to run the simulator, you need to install the python bindings for vlc:
+sudo pip install python-vlc
+"""
+import vlc
+import time
 
 
-class StreamSimulator:
-    def start(self):
+# TODO: listen on port 5502 for connection, then start to stream
+class StreamSimulator():
+    """
+    The StreamSimulator executes the following command: cvlc testfootage.mp4 --sout '#rtp{dst=127.0.0.1,port=5000,ptype-video=96,mux=ts}'
+    """
+    def __init__(self):
+        self.Instance = vlc.Instance()
+        self.player = self.Instance.media_player_new()
 
-        # Initialization GStreamer
-        GObject.threads_init()
-        gst.init(None)
+    def run(self):
+        footage = 'testfootage.mp4'
+        options = "sout=#rtp{dst=127.0.0.1,port=5000,ptype-video=96,mux=ts}"
+        media = self.Instance.media_new(footage, options)
+        self.player.set_media(media)
+        self.player.play()
 
-        # Create pipeline
-        pipeline = gst.Pipeline()
-
-        # Create elements for pipeline
-        source = gst.ElementFactory.make('filesrc', None)
-        demux = gst.ElementFactory.make('qtdemux', None)
-        queue = gst.ElementFactory.make('queue', None)
-        pay = gst.ElementFactory.make('rtpmp4vpay', None)
-        address = gst.ElementFactory.make('udpsink', None)
-
-        if not (source and demux and queue and pay and address):
-            print 'Niet'
-            exit(-1)
-
-        # Add properties to elements
-        source.set_property('location', 'test_footage1.mp4')
-        demux.set_property('name', 'd')
-        address.set_property('host', '127.0.0.1')
-        address.set_property('port', 5000)
-
-        # Add elements to pipeline
-        pipeline.add(source)
-        pipeline.add(demux)
-        pipeline.add(queue)
-        pipeline.add(pay)
-        pipeline.add(address)
-
-        # Link elelemts together
-        source.link(demux)
-        demux.link(queue)
-        queue.link(pay)
-        pay.link(address)
-
-        # Set pipeline to PLAYING state
-        pipeline.set_state(gst.State.PLAYING)
-
-        # Wait until error or EOS
-        bus = pipeline.get_bus()
-        msg = bus.timed_pop_filtered(gst.CLOCK_TIME_NONE, gst.MessageType.ERROR | gst.MessageType.EOS)
-        err, debug = msg.parse_error()
-        print err
-        print debug
-
-        # Free resources.
-        pipeline.set_state(gst.State.NULL)
-
-ss = StreamSimulator()
-ss.start()
+        # 5 is the 'stopped' state, 6 is the 'ended' state
+        while (self.player.get_state() != 6 and self.player.get_state() != 5):
+            time.sleep(1)
