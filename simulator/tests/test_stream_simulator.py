56582191b07cf0d2204eb052b82131e56c1a5b4f
tkgoemae <t.goemaere@hotmail.com>
2016-03-31 22:14:21 +0200

    Unit test stream simulator: start and stop

--- simulator/tests/test_stream_simulator.py
+++ simulator/tests/test_stream_simulator.py
@@ -1,5 +1,7 @@
 import unittest
 import sys
+from time import sleep
+
 sys.path.append("../src")
 from stream_simulator import StreamSimulator 
 
@@ -8,10 +10,13 @@ class TestStreamSimulator(unittest.TestCase):
     def __init__(self):
         self.stream_simulator = StreamSimulator()
 
-    def test_stream_thread_start(self):
+    def test_stream_thread_start_and_stop(self):
         self.stream_simulator.start()
         self.assertTrue(self.stream_simulator.isAlive())
+
         self.stream_simulator.stop_thread()
+        sleep(3)
+        self.assertFalse(self.stream_simulator.isAlive())
 
 test_class = TestStreamSimulator()
-test_class.test_stream_thread_start()
+test_class.test_stream_thread_start_and_stop()
