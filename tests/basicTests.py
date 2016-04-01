528215a88ce4021d2667d9f0096b0a4abb3a83b8
jonas <jonasleo.spanoghe@ugent.be>
2016-04-01 11:33:32 +0200

    remove obsolote tests

--- tests/basicTests.py
+++ tests/basicTests.py
@@ -3,10 +3,9 @@ import json
 import socket
 import struct
 import sys
+
 sys.path.append('../onboard/')
 from global_classes import Location, WayPoint, WayPointEncoder, WayPointQueue
-from navigation_handler import NavigationHandler
-
 
 class BasicTests(unittest.TestCase):
     def test_one(self):
@@ -15,36 +14,6 @@ class BasicTests(unittest.TestCase):
     def test_two(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
-
-    def test_path_handler(self):
-        waypoints = []
-        for i in range(0, 4):
-            tmp_loc = Location(i, i)
-            waypoints.append(WayPoint(location=tmp_loc, order=i))
-        path_message = {'MessageType': 'control', 'Message': 'path', 'Path': waypoints}
-        json_message = json.dumps(path_message, cls=WayPointEncoder)
-        message = json.loads(json_message)
-        waypoint_queue = []
-        path_handler = NavigationHandler.PathHandler(message, waypoint_queue)
-        path_handler.handle_packet()
-        for waypoint in path_handler.waypoint_queue:
-            if waypoint.location.longitude not in range(0, 4) or waypoint.location.latitude not in range(0, 4):
-                self.fail
-
-    def test_path_messages(self):
-        waypoints = []
-        for i in range(0, 4):
-            tmp_loc = Location(i, i)
-            waypoints.append(WayPoint(location=tmp_loc, order=i))
-        path_message = {'MessageType': 'control', 'Message': 'path', 'Path': waypoints}
-        json_message = json.dumps(path_message, cls=WayPointEncoder)
-        control_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        control_socket.connect("/tmp/uds_control")
-        control_socket.send(json_message)
-        raw_response = control_socket.recv(1024)
-        response = struct.unpack(">I", raw_response)[0]
-        control_socket.close()
-        self.assertEqual(response, 200)
 
     def test_waypoint_queue(self):
         random_list = [4, 3, 6, 1, 5, 2, 3]
